800e93533f57d3a86e164eb0039c1ab4db576964
Aisha Toichieva <aishatoichieva@gmail.com>
2022-05-18 10:44:50 +0600

    adedd

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 """Python code goes here"""
 def developer3method():
-    
+
     print("name: {}\n ".format("Aisha"))
     print("surname: {}\n ".format("Toichieva"))
-    print("email: {}\n ".format("aishatoichieva@mail.ru"))
-    print("cohort: {}\n ".format("CS freshman"))
+    print("email: {}\n ".format("aishatoichieva@gmail.com"))
+    print("cohort: {}\n ".format("CS fresh"))
     print("age: {}\n ".format("20"))
-    print("gender: {}\n ".format("female"))
-    print("birthdate: {}\n ".format("2nd jan"))
-    print("speciality: {}\n ".format("BackEnd"))
+    print("gender: {}\n ".format("Feale"))
+    print("birthdate: {}\n ".format("02.01.2002"))
+    print("speciality: {}\n ".format("Frontend"))
     print("country: {}\n ".format("Tajikistan"))
-
-
-
+    
+    
