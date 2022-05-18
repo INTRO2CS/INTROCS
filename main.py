d8a86c978695b959c9b9385dba06a810d48d7e90
Umidbek25 <>
2022-05-18 10:25:44 +0600

    Gender added

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ def developer1method():
     print("surname: {}\n ".format("Ulmasov"))
     print("email: {}\n ".format("umidbek.ulmasov_2025@gmail.com"))
     print("cohort: {}\n ".format("CS freshman"))
+    print("age: {}\n ".format("20"))
+    print("gender: {}\n ".format("Male"))
+
