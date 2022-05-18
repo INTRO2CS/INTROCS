ec5fb046593e42d8bffac5abf12d578f7cf7902f
Umidbek25 <>
2022-05-18 10:23:35 +0600

    Cohort added

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ def developer1method():
     print("name: {}\n ".format("Umidbek"))
     print("surname: {}\n ".format("Ulmasov"))
     print("email: {}\n ".format("umidbek.ulmasov_2025@gmail.com"))
+    print("cohort: {}\n ".format("CS freshman"))
