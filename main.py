07bf704aae09259fb64a190d757c15b081baa440
Umidbek25 <>
2022-05-18 10:23:03 +0600

    Email added

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ def developer1method():
 
     print("name: {}\n ".format("Umidbek"))
     print("surname: {}\n ".format("Ulmasov"))
- 
+    print("email: {}\n ".format("umidbek.ulmasov_2025@gmail.com"))
